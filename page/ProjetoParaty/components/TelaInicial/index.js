e55f670f8e6c2722e360e736cbc8e411ef3cbf1f
Filipe Alves <lipeqa@gmail.com>
2024-02-10 09:07:43 -0300

    App paraty/adicionado as rotas de navegação

---
+++ page/ProjetoParaty/components/TelaInicial/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function TelaInicial() {
+  return (
+    <View>
+      <Text>TelaInicial
+    </Text>
+    </View>
+  )
+}
--- page/ProjetoParaty/index.js
+++ page/ProjetoParaty/index.js
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import TelaInicial from './components/TelaInicial';
+import TelaHospedagem from './components/TelaHospedagem';
+import TelaPasseios from './components/TelaPasseios';
+import TelaRestaurantes from './components/TelaRestaurantes';
+
+
+const Stack = createStackNavigator();
+
 
 export default function ProjetoParaty() {
   return (
-    <View>
-      <Text>ProjetoParaty</Text>
-    </View>
+      <NavigationContainer independent={true}>  
+      {/*//adicione a propriedade independent={true} ao NavigationContainer interno para torná-lo independente do NavigationContainer pai.  */}
+          <Stack.Navigator>
+              <Stack.Screen name="Tela Inicial" component={TelaInicial} />
+              <Stack.Screen name="Tela Hospedagem" component={TelaHospedagem} />
+              <Stack.Screen name="Tela Passeios" component={TelaPasseios} />
+              <Stack.Screen name="Tela Restaurantes" component={TelaRestaurantes} />
+          </Stack.Navigator>
+      </NavigationContainer>
   )
 }
