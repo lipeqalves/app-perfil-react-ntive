351ba1fbcfde35265346a6cb5a5c1bf788a19423
Filipe Alves <lipeqa@gmail.com>
2024-02-10 08:47:40 -0300

    iniciando um novo projeto: App paraty

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ProjetoPerfil from './page/ProjetoPerfil'
 import Home from './page/Home';
+import ProjetoParaty from "./page/ProjetoParaty";
 const Stack = createStackNavigator();
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Projeto Perfil" component={ProjetoPerfil} />
+        <Stack.Screen name="Projeto Paraty" component={ProjetoParaty} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ page/ProjetoParaty/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ProjetoParaty() {
+  return (
+    <View>
+      <Text>ProjetoParaty</Text>
+    </View>
+  )
+}
